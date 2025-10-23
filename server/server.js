1cb74addc9a39ba342a71ce0146ca08ef0fe2bf4
Abhay Kesarwani <56496714+Abhay-Kesarwani-git@users.noreply.github.com>
2025-10-23 13:47:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,10 +29,3 @@ app.use('/api/ai', aiRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-  }
