7e8ce55997403e224c643ffd8d9bc35c59f7ebca
Abhay Kesarwani <56496714+Abhay-Kesarwani-git@users.noreply.github.com>
2025-10-23 14:03:23 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Banner from '../components/home/banner'
+import Banner from '../components/home/Banner'
 import Hero from '../components/home/Hero'
 import Feature from '../components/home/Feature'
 import Testimonial from '../components/home/Testimonial'
